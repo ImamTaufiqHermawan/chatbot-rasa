acb517bb5c4904ff3217f90c87b94f6f711c070a
imam.hermawan <imam.hermawan@gramedia.id>
2023-01-06 02:29:43 +0700

    generate dynamic response for action confirm

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,39 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+
+class ActionConfirm(Action):
+
+    def name(self) -> Text:
+        return "action_confirm"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        service = tracker.get_slots('service')
+        p_count = tracker.get_slots('p_count')
+
+        print(service, p_count)
+
+        dispatcher.utter_message(text=f"Hi there, you booked {p_count} {service} tickets. Please confirm your booking")
+
+        return []

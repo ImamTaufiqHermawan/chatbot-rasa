b56b31fc38a44fbaa6cc922aa73ce2c052b887f8
imam.hermawan <imam.hermawan@gramedia.id>
2023-01-09 04:23:35 +0700

    generate dynamic response for pizza order

--- actions/actions.py
+++ actions/actions.py
@@ -7,11 +7,13 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+from tkinter import EventType
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+from rasa_sdk.forms import FormAction
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
@@ -42,3 +44,27 @@ class ActionConfirm(Action):
         dispatcher.utter_message(text=f"Hi there, you booked {p_count} {service} tickets. Please confirm your booking")
 
         return []
+
+class ActionPizzaOrderForm(FormAction):
+
+    def name(self) -> Text:
+        return "action_pizza_order_form"
+
+    @staticmethod
+    def required_slots(tracker: "Tracker") -> List[Text]:
+        return ["pizza_size", "pizza_type", "pizza_amount"]
+
+    def submit(
+        self,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+
+        pizza_type = tracker.get_slots('pizza_type')
+        pizza_size = tracker.get_slots('pizza_size')
+        pizza_amount = tracker.get_slots('pizza_amount')
+        
+        print(pizza_type, pizza_size, pizza_amount)
+
+        dispatcher.utter_message(text=f"ok Great. Your order is {pizza_amount} {pizza_type} pizza in {pizza_size} size. Can you please confirm your order?")

f1790fedb93b255e81349e3efb6d095f97d4e631
imam.hermawan <imam.hermawan@gramedia.id>
2023-01-09 06:54:42 +0700

    fix pizza order custom action

--- actions/actions.py
+++ actions/actions.py
@@ -12,59 +12,28 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.forms import FormValidationAction
 
-from rasa_sdk.forms import FormAction
-class ActionHelloWorld(Action):
-
-    def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
-
-class ActionConfirm(Action):
-
-    def name(self) -> Text:
-        return "action_confirm"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        service = tracker.get_slots('service')
-        p_count = tracker.get_slots('p_count')
-
-        print(service, p_count)
-
-        dispatcher.utter_message(text=f"Hi there, you booked {p_count} {service} tickets. Please confirm your booking")
-
-        return []
-
-class ActionPizzaOrderForm(FormAction):
+class ActionPizzaOrderForm(FormValidationAction):
 
     def name(self) -> Text:
         return "action_pizza_order_form"
 
-    @staticmethod
-    def required_slots(tracker: "Tracker") -> List[Text]:
-        return ["pizza_size", "pizza_type", "pizza_amount"]
-
-    def submit(
+    async def required_slots(        
         self,
+        domain_slots: List[Text],
         dispatcher: "CollectingDispatcher",
         tracker: "Tracker",
-        domain: Dict[Text, Any],
-    ) -> List[EventType]:
+        domain: "DomainDict",
+    ) -> List[Text]:
+        pizza_type = tracker.slots.get('pizza_type')
+        pizza_amount = tracker.slots.get('pizza_amount')
+        pizza_size = tracker.slots.get('pizza_size')
 
-        pizza_type = tracker.get_slots('pizza_type')
-        pizza_size = tracker.get_slots('pizza_size')
-        pizza_amount = tracker.get_slots('pizza_amount')
-        
-        print(pizza_type, pizza_size, pizza_amount)
+        print(pizza_type, pizza_amount, pizza_size)
 
-        dispatcher.utter_message(text=f"ok Great. Your order is {pizza_amount} {pizza_type} pizza in {pizza_size} size. Can you please confirm your order?")
+        dispatcher.utter_message(
+            text=f"ok Great. Your order is {pizza_amount} {pizza_type} pizza in {pizza_size} size. Can you please confirm your order?")
+
+        return []
